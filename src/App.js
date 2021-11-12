4276a9d10725780df16741c9ec50ad48cf05f050
yota-g <64677279+yota-g@users.noreply.github.com>
2021-11-12 22:58:48 +0900

    learn about Atomic Design

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atom/button/PrimaryButton";
-import { SecondaryButton } from "./components/atom/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/Searchinput";
-import { UserCard } from "./components/organism/user/UserCard";
-import { DefaultLayout } from "./components/template/DefaultLayout";
-import { HeaderOnly } from "./components/template/HeaderOnly";
+// import { BrowserRouter } from "react-router-dom";
+// import { PrimaryButton } from "./components/atom/button/PrimaryButton";
+// import { SecondaryButton } from "./components/atom/button/SecondaryButton";
+// import { SearchInput } from "./components/molecules/Searchinput";
+// import { UserCard } from "./components/organism/user/UserCard";
+// import { DefaultLayout } from "./components/template/DefaultLayout";
+// import { HeaderOnly } from "./components/template/HeaderOnly";
+import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "じゃけぇ",
-  image: "https://source.unsplash.com/NE0XGVKTmcA",
-  email: "12345@aaa.com",
-  tel: "000-999-0000",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
+// const user = {
+//   name: "じゃけぇ",
+//   image: "https://source.unsplash.com/NE0XGVKTmcA",
+//   email: "12345@aaa.com",
+//   tel: "000-999-0000",
+//   company: {
+//     name: "テスト株式会社"
+//   },
+//   website: "https://google.com"
+// };
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      {/* <HeaderOnly> */}
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索する</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-        {/* </HeaderOnly> */}
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
+  // return (
+  // <BrowserRouter>
+  //   <DefaultLayout>
+  //     <PrimaryButton>テスト</PrimaryButton>
+  //     <SecondaryButton>検索する</SecondaryButton>
+  //     <br />
+  //     <SearchInput />
+  //     <UserCard user={user} />
+  //   </DefaultLayout>
+  // </BrowserRouter>
+  // );
 }
---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import { SearchInput } from "../molecules/Searchinput";
+import { UserCard } from "../organism/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `じゃけぇ${val}`,
+    image: "https://source.unsplash.com/NE0XGVKTmcA",
+    email: "12345@aaa.com",
+    tel: "000-999-0000",
+    company: {
+      name: "テスト株式会社"
+    },
+    website: "https://google.com"
+  };
+});
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <h2>ユーザー一覧</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+`;
